979ff4b2444cfb5780a5826c508a8a3c4fb8176d
RodrigoSanchez06 <rogerxdgta@ciencias.unam.mx>
2022-10-02 19:28:15 -0500

    add: builder falta presentar mas a detalle los componentes, de ahi en fuera todo bien

--- src/ConstructorDeNaves.java
+++ src/ConstructorDeNaves.java
@@ -1,12 +1,18 @@
+import java.util.Scanner;
+
+import componentes.SistemaDePropulsion.SistemaDePropulsion;
 import componentes.SistemaDePropulsion.ViajeIntercontinental;
 import componentes.SistemaDePropulsion.ViajeIntergalactico;
 import componentes.SistemaDePropulsion.ViajeInterplanetario;
+import componentes.armas.Armas;
 import componentes.armas.LaserDestructorDePlanetas;
 import componentes.armas.LaserSimple;
 import componentes.armas.MisilesDePlasma;
+import componentes.blindaje.Blindaje;
 import componentes.blindaje.BlindajeFortaleza;
 import componentes.blindaje.BlindajeReforzado;
 import componentes.blindaje.BlindajeSimple;
+import componentes.cabina.Cabina;
 import componentes.cabina.CabinaEjercito;
 import componentes.cabina.CabinaTripulacionPequennia;
 import componentes.cabina.CabinaUnPiloto;
@@ -41,6 +47,156 @@ public class ConstructorDeNaves {
     }
 
     public Nave armaTuNave() {
-        return null;
+        Nave nave = new Nave();
+        System.out.println("*****Comenzando Proceso del Armado de tu nave personalizada*****");
+        System.out.println("Comenzaremos con el sistema de armas de tu nave.");
+        nave.agregarComponente(colocaArmas());
+        System.out.println("Ahora sigue el blindaje de tu nave. ");
+        nave.agregarComponente(colocaBlindaje());
+        System.out.println("Ahora toca elegir la cabina que deseas.");
+        nave.agregarComponente(colocaCabina());
+        System.out.println("Finalmente el Sustema de propulsión.");
+        nave.agregarComponente(colocaSistemaDePropulsion());
+        return nave;
+    }
+
+    private Armas colocaArmas() {
+        Scanner entrada = new Scanner(System.in);
+        int opcion;
+        System.out.println("***¿Que Sistema de armas deseas?***"
+                + "\n 1 .- Laser Destructor de Planetas."
+                + "\n 2 .- Misiles de plasma."
+                + "\n 3 .- Laser Simple.");
+        while (true) {
+            try {
+                String opcionUsuario = entrada.nextLine();
+                opcion = Integer.parseInt(opcionUsuario);
+                if ((opcion > 0) && (opcion < 4)) {
+                    break;
+                } else {
+                    System.out.println("Ingresa una opcion valida"
+                            + "\n 1 .- Laser Destructor de Planetas."
+                            + "\n 2 .- Misiles de plasma."
+                            + "\n 3 .- Laser Simple.");
+                }
+            } catch (NumberFormatException ex) {
+                System.out.println("Ingresa una opcion valida"
+                        + "\n 1 .- Laser Destructor de Planetas."
+                        + "\n 2 .- Misiles de plasma."
+                        + "\n 3 .- Laser Simple.");
+            }
+        }
+        if (opcion == 1) {
+            return new LaserDestructorDePlanetas();
+        } else if (opcion == 2) {
+            return new MisilesDePlasma();
+        } else {
+            return new LaserSimple();
+        }
+    }
+
+    private Blindaje colocaBlindaje() {
+        Scanner entrada = new Scanner(System.in);
+        int opcion;
+        System.out.println("***¿Que Sistema de blindaje deseas?***"
+                + "\n 1 .- Blindaje Fortaleza."
+                + "\n 2 .- Blindaje Reforzado."
+                + "\n 3 .- Blindaje Simple.");
+        while (true) {
+            try {
+                String opcionUsuario = entrada.nextLine();
+                opcion = Integer.parseInt(opcionUsuario);
+                if ((opcion > 0) && (opcion < 4)) {
+                    break;
+                } else {
+                    System.out.println("Ingresa una opcion valida"
+                            + "\n 1 .- Blindaje Fortaleza."
+                            + "\n 2 .- Blindaje Reforzado."
+                            + "\n 3 .- Blindaje Simple.");
+                }
+            } catch (NumberFormatException ex) {
+                System.out.println("Ingresa una opcion valida"
+                        + "\n 1 .- Blindaje Fortaleza."
+                        + "\n 2 .- Blindaje Reforzado."
+                        + "\n 3 .- Blindaje Simple.");
+            }
+        }
+        if (opcion == 1) {
+            return new BlindajeFortaleza();
+        } else if (opcion == 2) {
+            return new BlindajeReforzado();
+        } else {
+            return new BlindajeSimple();
+        }
+    }
+
+    private Cabina colocaCabina() {
+        Scanner entrada = new Scanner(System.in);
+        int opcion;
+        System.out.println("***¿Que Cabina deseas?***"
+                + "\n 1 .- Cabina ejército."
+                + "\n 2 .- Cabina Tripulacion pequeña."
+                + "\n 3 .- Cambina de un piloto.");
+        while (true) {
+            try {
+                String opcionUsuario = entrada.nextLine();
+                opcion = Integer.parseInt(opcionUsuario);
+                if ((opcion > 0) && (opcion < 4)) {
+                    break;
+                } else {
+                    System.out.println("Ingresa una opcion valida"
+                            + "\n 1 .- Cabina ejército."
+                            + "\n 2 .- Cabina Tripulacion pequeña."
+                            + "\n 3 .- Cambina de un piloto.");
+                }
+            } catch (NumberFormatException ex) {
+                System.out.println("Ingresa una opcion valida"
+                        + "\n 1 .- Cabina ejército."
+                        + "\n 2 .- Cabina Tripulacion pequeña."
+                        + "\n 3 .- Cambina de un piloto.");
+            }
+        }
+        if (opcion == 1) {
+            return new CabinaEjercito();
+        } else if (opcion == 2) {
+            return new CabinaTripulacionPequennia();
+        } else {
+            return new CabinaUnPiloto();
+        }
+    }
+
+    private SistemaDePropulsion colocaSistemaDePropulsion() {
+        Scanner entrada = new Scanner(System.in);
+        int opcion;
+        System.out.println("***¿Que Cabina deseas?***"
+                + "\n 1 .- Viaje Intergalactico."
+                + "\n 2 .- Viaje Planetario."
+                + "\n 3 .- Viaje Intercontintental.");
+        while (true) {
+            try {
+                String opcionUsuario = entrada.nextLine();
+                opcion = Integer.parseInt(opcionUsuario);
+                if ((opcion > 0) && (opcion < 4)) {
+                    break;
+                } else {
+                    System.out.println("Ingresa una opcion valida"
+                            + "\n 1 .- Viaje Intergalactico."
+                            + "\n 2 .- Viaje Planetario."
+                            + "\n 3 .- Viaje Intercontintental.");
+                }
+            } catch (NumberFormatException ex) {
+                System.out.println("Ingresa una opcion valida"
+                        + "\n 1 .- Viaje Intergalactico."
+                        + "\n 2 .- Viaje Planetario."
+                        + "\n 3 .- Viaje Intercontintental.");
+            }
+        }
+        if (opcion == 1) {
+            return new ViajeIntergalactico();
+        } else if (opcion == 2) {
+            return new ViajeInterplanetario();
+        } else {
+            return new ViajeIntercontinental();
+        }
     }
 }
